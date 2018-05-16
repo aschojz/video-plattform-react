b9b55e888a30b5ddaba3ffdccafb2fa3cd790715
macmini <macmini@macminis-Mini.fritz.box>
2018-05-16 18:05:19 +0200

    lecture 27

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBfQIxwAW0T9i8CvWKezYmkcx_fZ-NjyT0';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => { 
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return(
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
